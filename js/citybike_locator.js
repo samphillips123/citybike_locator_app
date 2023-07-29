9ef6d4a85dbb24cc9db11a8dde994ef0291099c5
samphillips123 <sphillips123@gmail.com>
2023-07-29 09:53:21 -0700

    successfully displayed API data in browser

--- js/citybike_locator.js
+++ js/citybike_locator.js
@@ -8,6 +8,10 @@ window.onload = (e) => {
         console.log(err, ` ERROR`)
     }).then((cityBikeData) => {
         console.log(cityBikeData.networks[0])
+
+        document.querySelector('#network-name').innerHTML = cityBikeData.networks[0].name
+        document.querySelector('#network-city').innerHTML = cityBikeData.networks[0].location.city
+        document.querySelector('#network-country').innerHTML = cityBikeData.networks[0].location.country
     })
 
 
