68294a03517f972ab1265225712911a8a0136525
samphillips123 <sphillips123@gmail.com>
2023-07-29 09:21:37 -0700

    successfully set up first fetch

--- js/citybike_locator.js
+++ js/citybike_locator.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+// CityBike Locator app
+// API: https://api.citybik.es/v2/#filter
+
+window.onload = (e) => {
+    fetch(`https://api.citybik.es/v2/networks`).then((data) => {
+        return data.json()
+    }, (err) => {
+        console.log(err, ` ERROR`)
+    }).then((cityBikeData) => {
+        console.log(cityBikeData.networks[0])
+    })
+
+
+
+
+
+}
